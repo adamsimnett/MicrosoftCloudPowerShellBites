d0e14708abb3f714a16586173b288beca216eeb8
adamsimnett <149381645+adamsimnett@users.noreply.github.com>
2025-05-28 22:51:59 +0100

    Added option to specify tenant and subscription ID

--- AzurePowerShell/Authentication/AzureLogin.ps1
+++ AzurePowerShell/Authentication/AzureLogin.ps1
@@ -1,17 +1,27 @@
-function Connect-ToAzure {
+function Connect-AzurePwsh {
     param (
-        [int]$MaxRetries = 3,
-        [int]$DelaySeconds = 5
+        [int]$maxRetries = 3,
+        [int]$delaySeconds = 5,
+        [string]$tenantId,
+        [string]$subscriptionId
     )
 
     $retryCount = 0
     $connectionAchieved = $false
     $errorLog = @()
 
-    while (-not $connectionAchieved -and $retryCount -lt $MaxRetries) {
+    while (-not $connectionAchieved -and $retryCount -lt $maxRetries) {
         try {
-            Write-Host "Attempting to connect to Azure... (Try $($retryCount + 1)/$MaxRetries)" -ForegroundColor Cyan
-            $null = Connect-AzAccount -ErrorAction Stop
+            Write-Host "Attempting to connect to Azure... (Try $($retryCount + 1)/$maxRetries)" -ForegroundColor Cyan
+
+            if ($tenantId -or $subscriptionId) {
+                $null = Connect-AzAccount -TenantID $tenantId -Subscription $subscriptionId -ErrorAction Stop
+            } else {
+                $null = Connect-AzAccount -ErrorAction Stop
+            }
+            
+
+            # Run context command to check auth has succeeded, return info if it has            
             $context = Get-AzContext
             if ($context -and $context.Account) {
                 Write-Host "Successfully connected as $($context.Account)" -ForegroundColor Green
@@ -30,16 +40,16 @@ function Connect-ToAzure {
 
             Write-Warning $errorMessage
 
-            if ($retryCount -lt $MaxRetries) {
-                Write-Host "Retrying in $DelaySeconds seconds..." -ForegroundColor Yellow
-                Start-Sleep -Seconds $DelaySeconds
+            if ($retryCount -lt $maxRetries) {
+                Write-Host "Retrying in $delaySeconds seconds..." -ForegroundColor Yellow
+                Start-Sleep -Seconds $delaySeconds
             }
         }
     }
 
     # Collate and display error messages if authentication fails
     if (-not $connectionAchieved) {
-        Write-Error "Failed to authenticate to Azure after $MaxRetries attempts."
+        Write-Error "Failed to authenticate to Azure after $maxRetries attempts."
         $errorMessages | ForEach-Object { Write-Error $_ }
         throw "Azure authentication failed. See error messages above."
     }
